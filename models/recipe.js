d522d9a7f5ac0d5885ea9979edebbcdf2d5b5aa4
Mitch Henderson <shiftymitch@gmail.com>
2020-06-06 10:16:34 -0600

    Issue29 - Added recipe model, updated user model

---
+++ models/recipe.js
@@ -0,0 +1,30 @@
+module.exports = function (sequelize, DataTypes) {
+    const Recipe = sequelize.define("Recipe", {
+        
+        // description
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        
+        // title
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validation: {
+                len: [1, 255]
+            }
+        }
+    });
+
+    // Recipe/User association
+    Recipe.associate = function(models) {
+        Recipe.belongsTo(models.User, {
+          foreignKey: {
+            allowNull: false
+          }
+        });
+      };
+
+    return Recipe;
+};
--- models/user.js
+++ models/user.js
@@ -1,29 +1,32 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 const bcrypt = require("bcryptjs");
-// Creating our User model
+
 module.exports = function (sequelize, DataTypes) {
     const User = sequelize.define("User", {
-        // The email cannot be null, and must be a proper email before creation
-        email: {
+
+        //username
+        username: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
             validate: {
-                isEmail: true
+                isAlphanumeric: true
             }
         },
-        // The password cannot be null
+
+        //password
         password: {
             type: DataTypes.STRING,
             allowNull: false
         }
     });
-    // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+
+    // password validation
     User.prototype.validPassword = function (password) {
         return bcrypt.compareSync(password, this.password);
     };
-    // Hooks are automatic methods that run during various phases of the User Model lifecycle
-    // In this case, before a User is created, we will automatically hash their password
+
+    // password hashing
     User.addHook("beforeCreate", user => {
         user.password = bcrypt.hashSync(
             user.password,
@@ -31,5 +34,13 @@ module.exports = function (sequelize, DataTypes) {
             null
         );
     });
+
+    // Recipe/User association
+    User.associate = function(models) {
+        User.hasMany(models.Recipe, {
+          onDelete: "cascade"
+        });
+      };
+
     return User;
 };
