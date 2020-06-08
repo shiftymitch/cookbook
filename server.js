4158bbfde595dbf763cecc321155e70c23690ed1
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-06-08 15:07:32 -0600

    added handlebars engine to server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const exphbs = require("express-handlebars");
 const session = require("express-session");
 
 // require passport as configured
@@ -15,14 +16,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// Set Handlebars as the default template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Utilize sessions to keep track of the user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Require our routes
-require("./routes/api-routes");
-require("./routes/html-routes");
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
 
 // Sync the database and start server
 db.sequelize.sync({ force: true }).then(function () {
