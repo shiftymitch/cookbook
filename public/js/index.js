06b8985b8c966595b7775e70fbfe91f4206b28b8
nabeek <nabeek@gmail.com>
2020-06-13 10:57:23 -0600

    Added links to individual recipes

--- public/js/index.js
+++ public/js/index.js
@@ -2,24 +2,34 @@ $(document).ready(function () {
 
   getRandomRecipe()
 
-  //! send newRecipe & newIngredients on submit
+  // Get random recipes from 3rd party API and use data for homepage
   function getRandomRecipe() {
     $.get("/api/random-recipe").then(function (data) {
       console.log(data.recipes)
       $("#random-recipe-one-name").text(data.recipes[0].title);
+      $("#random-recipe-one-name").attr({
+        href: data.recipes[0].spoonacularSourceUrl,
+        target: "_blank"
+      });
       $("#random-recipe-one-img").attr({
         src: data.recipes[0].image,
         alt: data.recipes[0].title
       });
-      $("#random-recipe-one-summary").html(data.recipes[0].summary.substr(0, 250));
-
+      $("#random-recipe-one-summary").html(data.recipes[0].summary.replace(/^(.{300}[^\s]*).*/, "$1") + "...");
 
       $("#random-recipe-two-name").text(data.recipes[1].title);
+      $("#random-recipe-two-name").attr({
+        href: data.recipes[1].spoonacularSourceUrl,
+        target: "_blank"
+      });
       $("#random-recipe-two-img").attr({
         src: data.recipes[1].image,
         alt: data.recipes[1].title
       });
-      $("#random-recipe-two-summary").html(data.recipes[1].summary.substr(0, 250));
+      $("#random-recipe-two-summary").html(data.recipes[1].summary.replace(/^(.{300}[^\s]*).*/, "$1") + "...");
+    }).then(() => {
+      $("#random-box-one").addClass("box")
+      $("#random-box-two").addClass("box")
     });
   }
 
